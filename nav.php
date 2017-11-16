09baed9a819a50ec32206fcdf4ec0ac4eb621a2c
nicholashanoian <33403134+nicholashanoian@users.noreply.github.com>
2017-11-16 18:30:27 -0500

    top nav

--- nav.php
+++ nav.php
@@ -1,7 +1,48 @@
-<!-- ####################    Start of  Nav   ############################ -->
+`<!-- ####################    Start of  Nav   ############################ -->
 
 
 
+<nav id="topnav">
+    <ol>
+        <?php
+        
+        
+        
+        
+        print '<li class="';
+        if($path_parts['filename'] == 'roleplay') {
+            print 'activePage';}
+        print '">';
+        print '<a href="genre.php?genre=roleplay">RPGs</a>';
+        print '</li>';
+        
+        
+        print '<li class="';
+        if($path_parts['filename'] == 'shooters') {
+            print 'activePage';}
+        print '">';
+        print '<a href="genre.php?genre=shooter">Shooters</a>';
+        print '</li>';
+        
+        
+        print '<li class="';
+        if($path_parts['filename'] == 'sports') {
+            print 'activePage';}
+        print '">';
+        print '<a href="genre.php?genre=sports">Sports</a>';
+        print '</li>';
+        
+        
+        print '<li class="';
+        if($path_parts['filename'] == 'form') {
+            print 'activePage';}
+        print '">';
+        print '<a href="form.php">Join</a>';
+        print '</li>';
+        
+        ?>
+    </ol>
+</nav>
 
 
 
